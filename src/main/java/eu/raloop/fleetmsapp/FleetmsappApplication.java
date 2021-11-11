7a211ac53f021e23156894f1c1a4fdc4cb3f194d
Rares Lup <luprares@gmail.com>
2021-11-11 17:39:02 +0200

    First Commit

---
+++ src/main/java/eu/raloop/fleetmsapp/FleetmsappApplication.java
@@ -0,0 +1,13 @@
+package eu.raloop.fleetmsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetmsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetmsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/raloop/fleetmsapp/FleetmsappApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.raloop.fleetmsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetmsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
